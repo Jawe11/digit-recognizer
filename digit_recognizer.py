1c247cb46a5b37b7f211ed13c50d4b482abd0453
Jawe11 <janwerth@aol.de>
2020-05-01 15:50:28 +0200

    Update digit_recognizer.py
    testbild aus train datensatz  und wahren wert anzeigen

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -11,5 +11,16 @@ test = pd.read_csv("C:/Users/janwe/Documents/GitHub/digit-recognizer/test.csv",s
 train_label = train["label"]
 # print(train_label)
 
-train_data = train.drop("label", axis = 1)
-print(train_data)
+train_data = (train.drop("label", axis = 1))
+
+#code zum darstellen einer zahl 
+
+index_testbild = 676
+testdaten = train_data.iloc[index_testbild]
+testbild = testdaten.values.reshape(28,28)
+
+plt.imshow(testbild)
+plt.title("Angezeigte Zahl:" + str(train_label[index_testbild]))
+plt.show()
+
+print(testbild)
