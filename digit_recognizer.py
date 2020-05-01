44fcb5d9ee98d9d748154183281f50dc38a26fb5
Jawe11 <janwerth@aol.de>
2020-05-01 16:12:02 +0200

    Update digit_recognizer.py
    floating point conversion & beginn keras Modell

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,4 +1,5 @@
 import tensorflow as tf   # Tensorflow laden
+from tensorflow import keras #Keras laden
 import pandas as pd         # Pandas zur Datenanalyse
 import matplotlib.pyplot as plt #Pyplot zur graphischen Darstellung
 
@@ -9,18 +10,21 @@ test = pd.read_csv("C:/Users/janwe/Documents/GitHub/digit-recognizer/test.csv",s
 # print(test.head())
 
 train_label = train["label"]
-# print(train_label)
-
 train_data = (train.drop("label", axis = 1))
 
-#code zum darstellen einer zahl 
+train_data = train_data / 255.0 #conversion to floating point
+test_data = test / 255.0
+
+#code zum darstellen eines testbildes 
 
 index_testbild = 676
 testdaten = train_data.iloc[index_testbild]
 testbild = testdaten.values.reshape(28,28)
 
-plt.imshow(testbild)
+plt.imshow(testbild, cmap=plt.cm.binary) #testbild in SW darstellen
 plt.title("Angezeigte Zahl:" + str(train_label[index_testbild]))
 plt.show()
 
-print(testbild)
+# Aufbau Keras Modell
+
+model = keras.Sequential()
